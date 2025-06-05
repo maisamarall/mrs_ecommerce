5627efb3b9995d24aad160b4f5b2b1a0416eb2d7
Samara-Adorno <adornosamara165@gmail.com>
2025-06-04 22:44:38 -0300

    Fazendo o componente do footer do site

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
-
+import Footer from './components/FooterComponent.vue'
 </script>
 
 <template>
+  <div id="app" class="min-h-screen bg-gray-100">
+    <header>
+      <!-- Nav -->
+    </header>
 
+    <main class="flex-1 container mx-auto px-4 py-6">
+      <router-view />
+    </main>
+
+    <footer>
+      <Footer />
+    </footer>
+  </div>
 </template>
 
 <style scoped>
